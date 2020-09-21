52a241db2e42fa5bafb5540503ccc62f9d663e18
jstephend <stephenfishlmao@gmail.com>
2020-09-21 19:34:04 +0800

    test2

--- models/Connection.php
+++ models/Connection.php
@@ -1,9 +1,14 @@
 <?php
 
-class Connection {
-    function connect() {
-        $link = new PDO("mysql:host=localhost;dbname=contactrace", "root", "");
-		$link -> exec("set names utf8");
-		return $link;
+class Connection
+{
+    function connect()
+    {
+        if (isset($_ENV['CLEARDB_DATABASE_URL'])) {
+        } else {
+            $link = new PDO("mysql:host=localhost;dbname=contactrace", "root", "");
+            $link->exec("set names utf8");
+            return $link;
+        }
     }
 }
