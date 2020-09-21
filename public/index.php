e6263f908b8a3379eccbce9567884574cd0e6151
jstephend <stephenfishlmao@gmail.com>
2020-09-21 21:25:42 +0800

    test5

--- public/index.php
+++ public/index.php
@@ -108,6 +108,7 @@ switch ($routeInfo[0]) {
             $messages->show();
         }
 
+        var_dump([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
         $class = new $classname($db->connect());
         call_user_func_array([$class, $method], [$vars, $httpMethod]);
 
@@ -120,4 +121,3 @@ switch ($routeInfo[0]) {
         break;
 }
 ob_end_flush();
-?>
