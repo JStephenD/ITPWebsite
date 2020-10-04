8ec6c3d9c43bae2394cff85328045af339c55476
jstephend <stephenfishlmao@gmail.com>
2020-10-04 16:18:46 +0800

    change home page

--- views/home.php
+++ views/home.php
@@ -1,10 +1,4 @@
-<div style="margin-left: 50%; transform: translateX(-50%);">
-    <h1 style="color:lightsalmon">ERROR 404</h1>
-    <p>To sir:</p>
-    <p>localhost/heroproadmin is not the way the server is served</p>
-    <p>I served it using </p>
-    <p>php -S localhost:8000</p>
-    <p>this is so the routes would work</p>
-    <p>example: localhost/heroproadmin/citymunicipality will not work</p>
-    <p>but localhost:8000/citymunicipality will work</p>
+<div style="margin-left: 50%; transform: translate(-50%, 50%);">
+    <h1>Covid Trace Application</h1>
+    <h4>ITP - Website</h4>
 </div>
